0a97cb30595a62dd571138baf38535ea44dbc6eb
Zachary <zlake23@gmail.com>
2019-08-31 21:10:36 -0700

    Adds calls for temp, wind speed, city name, and humidity data

--- script.js
+++ script.js
@@ -19,7 +19,49 @@ function searchWeather(searchTerm) {
 }
 
 function init(resultFromServer) {
-    console.log(resultFromServer);
+    switch (resultFromServer.weather[0].main) {
+        case 'Clear':
+            document.body.style.backgroundImage = 'url("clear.jpg")';
+            break;
+
+        case 'Clouds':
+            document.body.style.backgroundImage = 'url("cloudy.jpg")';
+            break;
+
+        case 'Rain':
+        case 'Drizzle':
+        case 'Mist':
+            document.body.style.backgroundImage = 'url("rain.jpg")';
+            break;
+
+        case 'Thunderstorm':
+            document.body.style.backgroundImage = 'url("storm.jpg")';
+            break;
+
+        case 'Snow':
+            document.body.style.backgroundImage = 'url("snow.jpg")';
+            break;
+        
+            default:
+                break;
+    }
+
+    let weatherDescriptionHeader = document.getElementById('weatherDescriptionHeader');
+    let temperatureElement = document.getElementById('temperature');
+    let humidityElement = document.getElementById('humidity');
+    let windSpeedElement = document.getElementById('windSpeed');
+    let cityHeader = document.getElementById('cityHeader');
+    let weatherIcon = document.getElementById('documentIconImg');
+
+    weatherIcon.src = 'http://openweathermap.org/img/wn/' + resultFromServer.weather[0].icon + '.png';
+
+    let resultDescription = resultFromServer.weather[0].description;
+    weatherDescriptionHeader.innerText = resultDescription.charAt(0).toUpperCase() + resultDescription.slice(1);
+
+    temperatureElement.innerHTML = Math.floor(resultFromServer.main.temp) + '&#176';
+    windSpeedElement.innerHTML = 'Winds at ' + Math.floor(resultFromServer.wind.speed) + ' m/s';
+    cityHeader.innerHTML = resultFromServer.name;
+    humidityElement.innerHTML = 'Humidity levels at ' + resultFromServer.main.humidity + '%';
 }
 
 document.getElementById('searchBtn').addEventListener('click', () => {
